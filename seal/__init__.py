493a3362a150a4792e237f1a5d5d2e7539b54ced
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-19 17:27:55 +0200

    Add form to login

--- seal/__init__.py
+++ seal/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash, redirect, url_for
+from forms.registration import LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '78486cd05859fc8c6baa29c430f06638'
 
 
 @app.route("/")
@@ -9,6 +11,19 @@ def index():
     return render_template(
         "essentials/home.html",
         title="Home"
+    )
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'{form.username.data} logged!', 'success')
+        redirect(url_for('index'))
+    return render_template(
+        "essentials/login.html",
+        title="Login",
+        form=form
     )
 
 
