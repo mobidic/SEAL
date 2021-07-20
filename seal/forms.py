05cefaa3e70d89c8be70706e1feb3ab81a5442df
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-20 10:02:15 +0200

    Move forms into a single file

---
+++ seal/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
