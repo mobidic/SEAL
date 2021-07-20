935aee2f9b82621e88e87203a881b2d00949f631
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-20 13:13:18 +0200

    Reordering routes

--- seal/routes.py
+++ seal/routes.py
@@ -5,6 +5,9 @@ from seal.models import User
 from flask_login import login_user, current_user, logout_user
 
 
+################################################################################
+# Essentials pages
+
 @app.route("/")
 @app.route("/home")
 def index():
@@ -12,32 +15,6 @@ def index():
         "essentials/home.html",
         title="Home"
     )
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('about'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            flash(f'You are logged as: {user.username}!', 'success')
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for('index'))
-        else:
-            flash('Login unsucessful. Please check the username and/or password!', 'error')
-    return render_template(
-        "essentials/login.html",
-        title="Login",
-        form=form
-    )
-
-
-@app.route("/logout", methods=['GET', 'POST'])
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
 
 
 @app.route("/about")
@@ -54,3 +31,39 @@ def contact():
         "essentials/contact.html",
         title="Contact"
     )
+
+
+################################################################################
+
+
+################################################################################
+# Authentication related pages
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash(f'You are logged as: {user.username}!', 'success')
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsucessful. Please check the username and/or password!', 'error')
+    return render_template(
+        "authentication/login.html",
+        title="Login",
+        form=form
+    )
+
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+################################################################################
