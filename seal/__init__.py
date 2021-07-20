caf96df78dbd514e76ad7715b5a73dbbddd86779
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-20 10:03:08 +0200

    Initialise db (user & team)

--- seal/__init__.py
+++ seal/__init__.py
@@ -1,8 +1,48 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from forms.registration import LoginForm
-app = Flask(__name__)
+from seal.forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '78486cd05859fc8c6baa29c430f06638'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+teams = db.Table(
+    'teams',
+    db.Column(
+        'team_ID', db.Integer,
+        db.ForeignKey('team.id'), primary_key=True
+    ),
+    db.Column(
+        'user_ID', db.Integer,
+        db.ForeignKey('user.id'), primary_key=True
+    )
+)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(
+        db.String(20), unique=True,
+        nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), unique=True, nullable=False)
+    teams = db.relationship(
+        'Team', secondary=teams, lazy='subquery',
+        backref=db.backref('members', lazy=True)
+    )
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.image_file}')"
+
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teamname = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Team('{self.teamname}')"
 
 
 @app.route("/")
