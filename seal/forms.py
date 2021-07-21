76ce44c0d668677ae9a066f7d4e38be738f7d659
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-21 10:04:22 +0200

    Add form update

--- seal/forms.py
+++ seal/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Optional
+from flask_login import current_user
+from seal.models import User
 
 
 class LoginForm(FlaskForm):
@@ -14,3 +16,27 @@ class LoginForm(FlaskForm):
     )
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    mail = StringField(
+        'Mail',
+        validators=[Optional(), Email()]
+    )
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is already taken. Please choose another one.')
+
+    def validate_mail(self, mail):
+        if mail.data != current_user.mail:
+            user = User.query.filter_by(mail=mail.data).first()
+            if user:
+                raise ValidationError('That mail is already taken. Please choose another one.')
