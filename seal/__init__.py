ae12a2dbf84a50ddc4a9f544e875260f0b0831aa
Char-Al <charlesvangoethem+github@gmail.com>
2021-07-30 10:08:16 +0200

    Add APScheduler

--- seal/__init__.py
+++ seal/__init__.py
@@ -2,15 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_apscheduler import APScheduler
+
 
 app = Flask(__name__)
+scheduler = APScheduler()
+
+
+# Configure application option
 app.config['SECRET_KEY'] = '78486cd05859fc8c6baa29c430f06638'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///seal"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SCHEDULER_API_ENABLED'] = True
+
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+scheduler.init_app(app)
+scheduler.start()
+
 
 from seal import routes
