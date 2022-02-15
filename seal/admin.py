11d093d2436af2f196d6c0024cbd3d8c5e7ddde6
Char-Al <charlesvangoethem+github@gmail.com>
2022-02-15 11:04:09 +0100

    Add CustomView Class

--- seal/admin.py
+++ seal/admin.py
@@ -29,6 +29,31 @@ class AdminView(ModelView):
         return redirect(url_for('login', next=request.url))
 
 
+class CustomView(ModelView):
+
+    column_exclude_list = []
+    column_searchable_list = []
+    column_editable_list = []
+
+    def __init__(self, *args, **kwargs):
+        if 'column_exclude_list' in kwargs:
+            self.column_exclude_list = kwargs.pop('column_exclude_list')
+        if 'column_searchable_list' in kwargs:
+            self.column_searchable_list = kwargs.pop('column_searchable_list')
+        if 'column_editable_list' in kwargs:
+            self.column_editable_list = kwargs.pop('column_editable_list')
+        super(CustomView, self).__init__(*args, **kwargs)
+
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            return current_user.admin
+        return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('login', next=request.url))
+
+
 admin = Admin(app, index_view=MyAdminIndexView())
 admin.add_view(AdminView(User, db.session, category="Authentication"))
 admin.add_view(AdminView(Team, db.session, category="Authentication"))
