80bd8907d6522c1d0e45ac815a2d14136e6b8d73
Char-Al <charlesvangoethem+github@gmail.com>
2022-02-21 18:05:40 +0100

    Enhanced Mobidetails links: only if API key is defined & active in details for all NM

--- seal/__init__.py
+++ seal/__init__.py
@@ -18,7 +18,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///seal"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SCHEDULER_API_ENABLED'] = True
 app.config['SCHEDULER_JOB_DEFAULTS'] = {"coalesce": False, "max_instances": 2}
-app.config["API_KEY_MD"] = os.environ.get("API_KEY_MD")
+if os.environ.get("API_KEY_MD"):
+    app.config["API_KEY_MD"] = os.environ.get("API_KEY_MD")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
