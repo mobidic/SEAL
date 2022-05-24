5e8466eda583fc9f121880a4ba9ed8586e411cca
Char-Al <charlesvangoethem+github@gmail.com>
2022-05-24 11:03:20 +0200

    Add Timeout Logout

--- seal/__init__.py
+++ seal/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_apscheduler import APScheduler
-from flask_wtf import CSRFProtect
-from flask_migrate import Migrate
 import os
+from datetime import timedelta
+
+from flask import Flask, session, g
+from flask_apscheduler import APScheduler
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, current_user
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 
 app = Flask(__name__)
@@ -22,6 +24,7 @@ app.config['SCHEDULER_JOB_DEFAULTS'] = {"coalesce": False, "max_instances": 2}
 if os.environ.get("API_KEY_MD"):
     app.config["API_KEY_MD"] = os.environ.get("API_KEY_MD")
 
+# Plugins initialization
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -33,6 +36,15 @@ csrf.init_app(app)
 migrate = Migrate(app, db)
 
 app.logger.info(app.config)
+
+
+# Configure Timeout
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=20)
+    session.modified = True
+    g.user = current_user
 
 
 from seal import routes
