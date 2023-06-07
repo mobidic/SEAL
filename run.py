5ce5d09df9b7441855f5e087b41801def482b9fa
Char-Al <charlesvangoethem+github@gmail.com>
2023-06-07 09:45:19 +0200

    Fix Security issues: Flask app is run in debug mode

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
+import argparse
 from seal import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser(description="SEAL: Simple, Efficient And Lite database for NGS")
+    group_input = parser.add_argument_group('Options')
+    group_input.add_argument(
+        '-d',
+        '--debug',
+        default=False,
+        action='store_true',
+        help="Active debug mode"
+    )
+    group_input.add_argument(
+        '-p',
+        '--port',
+        default=5000,
+        help="Choose port for SEAL"
+    )
+    args = parser.parse_args()
+    app.run(debug=args.debug, port=args.port)
